f05895b388bc84260f7ba71c50871a435d4cdceb
张泽文 <zhangzewen@pintechs.com>
2024-05-22 14:49:30 +0800

    feat:添加图标

--- electron/main.ts
+++ electron/main.ts
@@ -31,7 +31,7 @@ export let win: BrowserWindow | null;
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    icon: path.join(process.env.VITE_PUBLIC, "icon.png"),
     webPreferences: {
       preload: path.join(__dirname, "preload.mjs"),
     },
